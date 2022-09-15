5776176c5ed620a5533c4be7f41065f0fc23a68e
Md. Arafat Hossan Lisan <96826879+arafatlsn@users.noreply.github.com>
2022-09-16 01:40:20 +0600

    Update DbConnect.js

--- Utility/DbConnect.js
+++ Utility/DbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const dbConnect = () => {
-  mongoose.connect("mongodb+srv://todo-user:42U2jTjrxOXBip7w@cluster0.moy4n.mongodb.net/Male_Fashion?retryWrites=true&w=majority")
+  mongoose.connect(process.env.DB_URL)
   .then(() => console.log("db connected"))
   .catch(err => console.log("DBConnect.js line 6",err.message))
 }
